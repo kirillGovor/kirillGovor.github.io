219f905a8c1abefe1df4c35fb75cb5c9c28ea22e
kirillGovor <bns223@mail.ru>
2019-01-31 14:48:11 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e28fde6851f0ed2bd850da2b0d73efbc.js"
+  "/precache-manifest.8ca461ff2f1b57ac95335fb7a42588c5.js"
 );
 
 workbox.clientsClaim();
